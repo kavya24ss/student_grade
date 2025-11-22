4f3ebbbb8580dfa0c0b3156884da0660de7d16cb
kavya24ss <sagikavyasri@gmail.com>
2025-11-22 12:11:00 +0530

    Update student_grade.py

--- student_grade.py
+++ student_grade.py
@@ -3,15 +3,11 @@ if len(sys.argv) != 6:
     print("ERROR: Please provide 5 subject marks as parameters.")
     print("Usage: python3 marks.py <SUB1> <SUB2> <SUB3> <SUB4> <SUB5>")
     sys.exit(1)
-# Reading marks from Jenkins parameters
+    
 marks = list(map(float, sys.argv[1:6]))
-print("\n=== INPUT RECEIVED FROM JENKINS PARAMETERS ===")
 print("Marks:", marks)
-# Calculate average
 avg = sum(marks) / 5
-# Very good marks > 90
-vg_marks = [m for m in marks if m > 90]
-# Grade logic
+
 if avg >= 90:
     grade = "A"
 elif avg >= 75:
@@ -22,8 +18,6 @@ elif avg >= 40:
     grade = "D"
 else:
     grade = "Fail"
-# Output
-print("\n===== RESULT =====")
+
 print("Average:", avg)
-print("Very Good Marks (>90):", vg_marks if vg_marks else "None")
-print("Grade:", grade)
+print("Grade:", grade)'
